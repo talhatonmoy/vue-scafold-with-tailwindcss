83519fc882e8764818450db52a6fbeaa424e7423
talhatonmoy <talha.tonmoy.ck@gmail.com>
2023-12-06 14:42:50 +0600

    watch, computed

--- state-management-vue/src/App.vue
+++ state-management-vue/src/App.vue
@@ -1,65 +1,44 @@
 <script setup>
-import { ref, reactive, computed } from 'vue'
+import { ref, reactive, watch, watchEffect } from 'vue'
 
-
-const fruitsNameWithPrice = ref([
-  { name: 'apple', price: 10.5 },
-  { name: 'banana', price: 15 },
-  { name: 'jackfruit', price: 20 },
-  { name: 'mango', price: 40 },
-])
-
-const fname = ref('')
-const fprice = ref('')
-
-const add = () => {
-
-  if (fname.value !== '' && fprice.value !== '') {
-    fruitsNameWithPrice.value.push({
-      name: fname.value, price: fprice.value
-    })
-  }
-
-  fname.value = ''
-  fprice.value = ''
-}
-
-
-const totalPrice = computed(() => {
-  let total = 0
-  for (let i = 0; i < fruitsNameWithPrice.value.length; i++) {
-    total += fruitsNameWithPrice.value[i].price
-  }
-  return total
+const post = reactive({
+  title: ''
 })
 
+const postId = ref(1)
 
+/**
+ * Using Async Awit
+ */
+
+watch(postId, async (newValue, oldValue) => {
+  post.title = 'Loading.....'
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${newValue}`)
+  const json = await response.json()
+  post.title = json.title
+}, { immediate: true })
+
+
+/**
+ * Using Fetch API Promise
+ */
+// watch(postId, (newValue, oldValue) => {
+//   post.title = 'Loading.....'
+//   fetch(`https://jsonplaceholder.typicode.com/posts/${newValue}`)
+//     .then((response) => response.json())
+//     .then((json) => {
+//       post.title = json.title
+//     })
+// }, { immediate: true })
 
 
 </script>
 
 <template>
-  <div class="container mt-10 mx-auto bg-gray-100 space-y-3 rounded-sm p-3">
-    <div class="flex space-x-2">
-      <label for="">Fruit Name</label>
-      <input type="text" class="p-1.5 rounded-md" name="" v-model="fname" id="">
-    </div>
-
-    <div class="flex space-x-2">
-      <label for="">Fruit Price</label>
-      <input type="number" class="p-1.5 rounded-md" name="" v-model="fprice" id="">
-    </div>
-
-    <button @click="add" class=" rounded-md py-1.5 px-2 bg-blue-500 text-white">Add Fruit</button>
-
-    <div class="flex space-x-2">
-      <label for="">Total Price: {{ totalPrice }}</label>
-    </div>
-
-  </div>
-
-  <div class="container mx-auto ">
-    {{ fruitsNameWithPrice }}
+  <div class="container mx-auto space-y-2 bg-gray-100 p-5 rounded-lg w-1/3 mt-20">
+    <p>Post Title: {{ post.title }}</p>
+    <p>Post Id: {{ postId }} </p>
+    <input type="text" v-model="postId">
   </div>
 </template>
 
---
+++ state-management-vue/src/components/Computed.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { ref, reactive, computed } from 'vue'
+
+
+const fruitsNameWithPrice = ref([
+    { name: 'apple', price: 10.5 },
+    { name: 'banana', price: 15 },
+    { name: 'jackfruit', price: 20 },
+    { name: 'mango', price: 40 },
+])
+
+const fname = ref('')
+const fprice = ref('')
+
+const add = () => {
+
+    if (fname.value !== '' && fprice.value !== '') {
+        fruitsNameWithPrice.value.push({
+            name: fname.value, price: fprice.value
+        })
+    }
+
+    fname.value = ''
+    fprice.value = ''
+}
+
+
+const totalPrice = computed(() => {
+    let total = 0
+    for (let i = 0; i < fruitsNameWithPrice.value.length; i++) {
+        total += fruitsNameWithPrice.value[i].price
+    }
+    return total
+})
+
+
+
+
+</script>
+
+<template>
+    <div class="container mt-10 mx-auto bg-gray-100 space-y-3 rounded-sm p-3">
+        <div class="flex space-x-2">
+            <label for="">Fruit Name</label>
+            <input type="text" class="p-1.5 rounded-md" name="" v-model="fname" id="">
+        </div>
+
+        <div class="flex space-x-2">
+            <label for="">Fruit Price</label>
+            <input type="number" class="p-1.5 rounded-md" name="" v-model="fprice" id="">
+        </div>
+
+        <button @click="add" class=" rounded-md py-1.5 px-2 bg-blue-500 text-white">Add Fruit</button>
+
+        <div class="flex space-x-2">
+            <label for="">Total Price: {{ totalPrice }}</label>
+        </div>
+
+    </div>
+
+    <div class="container mx-auto ">
+        {{ fruitsNameWithPrice }}
+    </div>
+</template>
+
+<style scoped></style>
